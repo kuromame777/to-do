dbc9f33897b7102a59e12e6347f5dfe089315f14
kuromame777 <hagakure80@yahoo.ne.jp>
2025-09-27 22:17:29 +0900

    Add files via upload

---
+++ public/sw.js
@@ -0,0 +1,30 @@
+// Service Worker for PWA
+const CACHE_NAME = 'todo-app-v1'
+const urlsToCache = [
+  '/',
+  '/static/js/bundle.js',
+  '/static/css/main.css',
+  '/manifest.json'
+]
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  )
+})
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // キャッシュがあれば返す
+        if (response) {
+          return response
+        }
+        return fetch(event.request)
+      })
+  )
+})
+
+
